ab1819f08d5e1e781dd50e251f40d1ed8f3c24f2
LowBrain <83059975+lowbrain@users.noreply.github.com>
2022-12-25 13:31:04 +0900

    PWA用のアイコン追加

--- vite.config.js
+++ vite.config.js
@@ -8,12 +8,34 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [
     vue(), 
-    VitePWA({registerType: 'autoUpdate'})
+    VitePWA({
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      registerType: 'autoUpdate', 
+      devOptions: { enabled: true },
+      manifest: {
+        name: 'pwa-vue',
+        short_name: 'pwa-vue',
+        description: 'pwa-vue',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'icons/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
-  base : "./",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  base : "./"
 })
