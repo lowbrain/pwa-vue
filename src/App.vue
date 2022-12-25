760ea9717e3b42847874b754dff6d05851956237
LowBrain <83059975+lowbrain@users.noreply.github.com>
2022-12-25 13:47:54 +0900

    相対パスの試験

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,8 @@ import HelloWorld from './components/HelloWorld.vue'
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/" append>Home</RouterLink>
+        <RouterLink to="/about" append>About</RouterLink>
       </nav>
     </div>
   </header>
